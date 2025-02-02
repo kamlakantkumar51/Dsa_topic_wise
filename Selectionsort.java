6bad9a2c74e93e184b808d01fcbebe508ad25082
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-02-02 16:52:50 +0530

    Create Selectionsort.java

---
+++ Selectionsort.java
@@ -0,0 +1,23 @@
+class Main{
+    public static void selectionSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int minPosition = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minPosition]>arr[j]){
+                    minPosition =j;
+                }
+            }
+            //swapping...
+            int temp = arr[minPosition];
+            arr[minPosition] = arr[i];
+            arr[i]= temp;
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {5,4,1,3,2};
+        selectionSort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Sorted array is :"+arr[i]);
+        }
+    }
+}
